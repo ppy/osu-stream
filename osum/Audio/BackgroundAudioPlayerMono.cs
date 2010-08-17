484f51d19d4cd9e9df022cfd203496e2479bcf6e
Dean Herbert <pe@ppy.sh>
2010-08-18 00:24:02 +0800

    Empty implementation of backgroundAudio for mono.

---
+++ osum/Audio/BackgroundAudioPlayerMono.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace osum.Audio
+{
+    //todo: implement
+    class BackgroundAudioPlayerMono : IBackgroundAudioPlayer
+    {
+        public BackgroundAudioPlayerMono()
+        {
+        }
+
+        public float CurrentVolume
+        {
+            get { return 0; }
+        }
+
+        public bool Play()
+        {
+            return false;
+        }
+    }
+}
