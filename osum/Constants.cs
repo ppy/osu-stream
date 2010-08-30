db627bc856085c8b9af598d436c138e54307a6de
peppy <pe@ppy.sh>
2010-08-30 14:17:32 +0800

    Unnecessary constants.

--- osum/Constants.cs
+++ osum/Constants.cs
@@ -4,8 +4,5 @@ namespace osum
     public static class Constants
     {
         public const double SIXTY_FRAME_TIME = (double)1000 / 60;
-
-        public const int GamefieldDefaultWidth = 1024;
-        public const int GamefieldDefaultHeight = 768;
     }
 }
