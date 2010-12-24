721e49c57cab32a2f9814b4695f51bb7eeb9a067
Dean Herbert <pe@ppy.sh>
2010-12-24 16:05:46 +0900

    Add a back button on ranking screen (at least it isn't totally blank now!)

--- osum/GameModes/Ranking.cs
+++ osum/GameModes/Ranking.cs
@@ -1,5 +1,10 @@
 using System;
 using osum.GameplayElements.Scoring;
+using osum.Graphics.Sprites;
+using osum.Graphics.Skins;
+using osum.Helpers;
+using OpenTK;
+using OpenTK.Graphics;
 namespace osum.GameModes
 {
 	public class Ranking : GameMode
@@ -8,7 +13,15 @@ namespace osum.GameModes
 		
 		internal override void Initialize()
 		{
+			//add a temporary button to allow returning to song select
+			pSprite backButton = new pSprite(TextureManager.Load("menu-back"),FieldTypes.StandardSnapBottomLeft, OriginTypes.BottomLeft,
+			                         ClockTypes.Game, Vector2.Zero, 1, true, new Color4(1,1,1,0.4f));
 			
+			backButton.OnClick += delegate {
+				backButton.UnbindAllEvents();
+				Director.ChangeMode(OsuMode.SongSelect);
+			};
+			spriteManager.Add(backButton);
 		}
 		
 		public Ranking()
