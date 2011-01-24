936d3a3a4b030fd5796ba67f205fd3e8877317f4
Dean Herbert <pe@ppy.sh>
2011-01-24 16:35:24 +0800

    Hold circles have a progress thingy!

--- osum/Graphics/Drawables/CircularProgress.cs
+++ osum/Graphics/Drawables/CircularProgress.cs
@@ -1,5 +1,7 @@
 using System;
 using osum.Graphics.Sprites;
+using osum.Graphics.Drawables;
+using osum.Helpers;
 #if IPHONE
 using OpenTK.Graphics.ES11;
 using MonoTouch.Foundation;
@@ -27,13 +29,13 @@ using ShaderParameter = OpenTK.Graphics.ES11.All;
 using ErrorCode = OpenTK.Graphics.ES11.All;
 using TextureEnvParameter = OpenTK.Graphics.ES11.All;
 using TextureEnvTarget =  OpenTK.Graphics.ES11.All;
-using osum.Graphics.Drawables;
-using osum.Helpers;
 #else
 using OpenTK.Input;
 using OpenTK.Graphics.OpenGL;
 using System.Drawing;
 using osum.Input;
+using OpenTK.Graphics;
+using OpenTK;
 #endif
 
 
@@ -41,66 +43,87 @@ namespace osum.Graphics.Drawables
 {
 	internal class CircularProgress : pDrawable
 	{
-		public CircularProgress()
+        internal float Progress;
+        internal float Radius;
+
+        public CircularProgress(Vector2 position, float radius, bool alwaysDraw, float drawDepth, Color4 colour)
 		{
-			AlwaysDraw = true;
+            AlwaysDraw = alwaysDraw;
+            DrawDepth = drawDepth;
+            StartPosition = position;
+            Position = position;
+            Radius = radius;
+            Colour = colour;
+            Field = FieldTypes.Standard;
 		}
 		
 		public override void Dispose()
 		{
 		}
 		
-		public override void Draw()
+		public override bool Draw()
 		{
-			base.Draw();
-			
-			float resolution = 0.1f;
-            float startAngle = (float) (-Math.PI/2);
-            
-            float endAngle = (float)((((float)Clock.Time / 1000) % 1) * (2 * Math.PI) + startAngle);
-
-            int parts = (int)((endAngle - startAngle) / resolution);
-			
-			float da = (endAngle - startAngle) / parts;
-            
-            float[] vertices = new float[parts * 2 + 2];
-            float[] colours = new float[parts * 4 + 4];
-
-            float radius = 1000 * ((Clock.Time / 1000f) % 1);
-
-            float xsc = 512;
-            float ysc = 384;
-
-            vertices[0] = xsc;
-            vertices[1] = ysc;
-			
-			colours[0] = 1;
-			colours[1] = 1;
-			colours[2] = 1;
-			colours[3] = 0.5f;
-
-            float a = startAngle;
-            for (int v = 1; v < parts + 1; v++)
+            if (base.Draw())
             {
-                vertices[v * 2] = (float)(xsc + Math.Cos(a)*radius);
-                vertices[v * 2 + 1] = (float)(ysc + Math.Sin(a)*radius);
-                a += da;
 
-                colours[v * 4] = 1;
-                colours[v * 4 + 1] = 1;
-                colours[v * 4 + 2] = 1;
-                colours[v * 4 + 3] = 0.5f;
+                Color4 c = AlphaAppliedColour;
+
+                Console.WriteLine(c.ToString());
+
+                float resolution = 0.1f;
+                float startAngle = (float)(-Math.PI / 2);
+                float cappedProgress = pMathHelper.ClampToOne(Progress);
+
+                float endAngle = (float)(cappedProgress * Math.PI * 2f + startAngle);
+
+                int parts = (int)((endAngle - startAngle) / resolution);
+
+                float da = (endAngle - startAngle) / parts;
+
+                parts++;
+
+                float[] vertices = new float[parts * 2 + 2];
+                float[] colours = new float[parts * 4 + 4];
+
+                float radius = Radius * GameBase.WindowRatio;
+                Vector2 pos = FieldPosition;
+
+                vertices[0] = pos.X;
+                vertices[1] = pos.Y;
+
+                colours[0] = c.R;
+                colours[1] = c.G;
+                colours[2] = c.B;
+                colours[3] = c.A * Progress;
+
+                float a = startAngle;
+                for (int v = 1; v <= parts; v++)
+                {
+                    vertices[v * 2] = (float)(pos.X + Math.Cos(a) * radius);
+                    vertices[v * 2 + 1] = (float)(pos.Y + Math.Sin(a) * radius);
+                    a += da;
+
+                    colours[v * 4] = c.R;
+                    colours[v * 4 + 1] = c.G;
+                    colours[v * 4 + 2] = c.B;
+                    colours[v * 4 + 3] = c.A * (0.1f + 0.4f * ((float)v/parts));
+                }
+
+                GL.Enable(EnableCap.ColorArray);
+                GL.Enable(EnableCap.VertexArray);
+
+                GL.VertexPointer(2, VertexPointerType.Float, 0, vertices);
+                GL.ColorPointer(4, ColorPointerType.Float, 0, colours);
+                GL.DrawArrays(BeginMode.TriangleFan, 0, parts + 1);
+
+                GL.Disable(EnableCap.ColorArray);
+                GL.Disable(EnableCap.VertexArray);
+
+                return true;
             }
 
-            GL.EnableClientState(EnableCap.ColorArray);
-            //GL.EnableClientState(EnableCap.VertexArray);
+            return false;
 
-		    //GL.VertexPointer(2,VertexPointerType.Float, 0, vertices);
-            GL.ColorPointer(4, ColorPointerType.Float, 0,colours);
-            GL.DrawArrays(BeginMode.TriangleFan, 0, parts + 1);
-            
-            GL.DisableClientState(EnableCap.ColorArray);
-            //GL.DisableClientState(EnableCap.VertexArray);
 		}
 	}
 }
