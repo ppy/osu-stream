e523968ba9313826713f47d1acb51f888599028d
Dean Herbert <pe@ppy.sh>
2011-01-25 23:31:18 +0800

    Limit how often the debug overlay is drawn.

--- osum/Support/DebugOverlay.cs
+++ osum/Support/DebugOverlay.cs
@@ -16,6 +16,9 @@ namespace osum.Support
         internal static pText fpsDisplay;
         private static double weightedAverageFrameTime;
 
+        static double lastUpdateTime;
+        static bool updateFrame;
+
         static internal void Update()
         {
             if (fpsDisplay == null)
@@ -31,15 +34,34 @@ namespace osum.Support
             weightedAverageFrameTime = weightedAverageFrameTime * 0.98 + GameBase.ElapsedMilliseconds * 0.02;
             double fps = (1000 / weightedAverageFrameTime);
 
+            lastUpdateTime += GameBase.ElapsedMilliseconds;
+
+            if (lastUpdateTime > 500)
+            {
+                lastUpdateTime = 0;
+                updateFrame = true;
+            }
+            else
+            {
+                updateFrame = false;
+            }
+
+
+            if (updateFrame)
+            {
+
 #if DEBUG
-            fpsDisplay.Colour = fps < 50 ? Color.OrangeRed : Color.GreenYellow;
-            fpsDisplay.Text = String.Format("{0:0}fps Game:{1:#,0}ms Mode:{4:#,0} Audio:{2:#,0}ms {3}", Math.Round(fps),
-                                            Clock.Time, Clock.AudioTime, Player.Autoplay ? "AP" : "", Clock.ModeTime);
+                fpsDisplay.Colour = fps < 50 ? Color.OrangeRed : Color.GreenYellow;
+                fpsDisplay.Text = String.Format("{0:0}fps Game:{1:#,0}ms Mode:{4:#,0} Audio:{2:#,0}ms {3}",
+                                                Math.Round(fps),
+                                                Clock.Time, Clock.AudioTime, Player.Autoplay ? "AP" : "", Clock.ModeTime);
 #endif
+            }
         }
 
         internal static void AddLine(string s)
         {
+            if (updateFrame)
 #if DEBUG
             fpsDisplay.Text += "\n" + s;
 #endif
