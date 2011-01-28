8edb5c1a27efa792d12da3197c5d521401f57329
Dean Herbert <pe@ppy.sh>
2011-01-28 15:39:52 +0800

    Circle size down 5% more for echo.

--- osum/GameplayElements/DifficultyManager.cs
+++ osum/GameplayElements/DifficultyManager.cs
@@ -11,7 +11,7 @@ namespace osum.GameplayElements
 		private const float HitObjectRadiusSprite = 64;
 		internal const float HitObjectSolidRatio = HitObjectRadiusSolid / HitObjectRadiusSprite;
 		
-		public static float HitObjectSizeModifier = 0.9f;
+		public static float HitObjectSizeModifier = 0.85f;
 
 		/// <summary>
         /// Radius of hitObjects in the current gamefield.
