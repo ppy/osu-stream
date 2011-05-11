9eeec49e4efaa5117edd4327db3115118653d6a0
Dean Herbert <pe@ppy.sh>
2011-05-11 11:53:40 +0800

    Fix NativeText rendering implementation for iOS devices to correctly measure unknown size strings.

--- osum/Graphics/Renderers/NativeTextRendererIphone.cs
+++ osum/Graphics/Renderers/NativeTextRendererIphone.cs
@@ -9,6 +9,7 @@ using OpenTK.Graphics.ES11;
 using System.Drawing;
 using OpenTK;
 using osum.Graphics.Sprites;
+
 namespace osum.Graphics.Renderers
 {
     unsafe internal class NativeTextRendererIphone : NativeTextRenderer
@@ -21,10 +22,20 @@ namespace osum.Graphics.Renderers
         internal override pTexture CreateText(string text, float size, OpenTK.Vector2 restrictBounds, OpenTK.Graphics.Color4 Color4, bool shadow, bool bold, bool underline, TextAlignment alignment, bool forceAa, out OpenTK.Vector2 measured, OpenTK.Graphics.Color4 background, OpenTK.Graphics.Color4 border, int borderWidth, bool measureOnly, string fontFace)
         {
             UIFont font = bold ? UIFont.BoldSystemFontOfSize(size) : UIFont.SystemFontOfSize(size);
-			
-			
+
+            NSString nsstr = new NSString(text);
+            SizeF actualSize = SizeF.Empty;
+
 			if (restrictBounds == Vector2.Zero)
-				restrictBounds = new Vector2((GameBase.NativeSize.Width > 512 ? 1024 : 512) * GameBase.ScaleFactor,64 * GameBase.ScaleFactor);
+            {
+                actualSize = nsstr.StringSize(font);
+                restrictBounds = new Vector2(actualSize.Width, actualSize.Height);
+            }
+            else if (restrictBounds.Y == 0)
+            {
+                actualSize = nsstr.StringSize(font, restrictBounds.X, UILineBreakMode.WordWrap);
+                restrictBounds = new Vector2(actualSize.Width, actualSize.Height);
+            }
 
             int width = TextureGl.GetPotDimension((int)restrictBounds.X);
             int height = TextureGl.GetPotDimension((int)restrictBounds.Y);
@@ -33,23 +44,20 @@ namespace osum.Graphics.Renderers
 
             byte[] data = new byte[height * width];
 
-            SizeF actualSize = SizeF.Empty;
-
             fixed (byte* dataPtr = data)
             {
-
                 CGBitmapContext context = new CGBitmapContext((IntPtr)dataPtr, width, height, 8, width, colorSpace,CGImageAlphaInfo.None);
 
                 colorSpace.Dispose();
-    
+
                 context.SetGrayFillColor(1, 1);
                 context.TranslateCTM(0, height);
                 context.ScaleCTM(1, -1);
 
                 UIGraphics.PushContext(context);
-                
-                actualSize = new NSString(text).DrawString(new RectangleF(0,0,restrictBounds.X,restrictBounds.Y),font, UILineBreakMode.TailTruncation,  UITextAlignment.Left);
-                
+
+                actualSize = nsstr.DrawString(new RectangleF(0,0,restrictBounds.X,restrictBounds.Y),font, UILineBreakMode.TailTruncation,  UITextAlignment.Left);
+
                 UIGraphics.PopContext();
 
                 measured = new OpenTK.Vector2(actualSize.Width, actualSize.Height);
