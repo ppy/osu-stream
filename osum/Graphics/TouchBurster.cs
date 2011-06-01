b38cc71fd0c1e6ac2880db12fb1333668ce4b889
Dean Herbert <pe@ppy.sh>
2011-06-01 15:41:37 +0800

    Reduce rate of transformation ctoring in TouchBurster, and add drag support.

--- osum/Graphics/TouchBurster.cs
+++ osum/Graphics/TouchBurster.cs
@@ -23,34 +23,77 @@ namespace osum.Graphics
             {
                 pSprite burst = new pSprite(TextureManager.Load(OsuTexture.mouse_burst), FieldTypes.Standard, OriginTypes.Centre, ClockTypes.Game, Vector2.Zero, 1, true, Color4.White);
                 burst.Additive = true;
+                burst.Alpha = 0;
                 burstSprites.Add(burst);
                 spriteManager.Add(burst);
+
+                burst.RemoveOldTransformations = false;
+
+                //make transformations beforehand to avoid creating many.
+                burst.Transform(new Transformation() { Type = TransformationType.Movement },
+                    new Transformation() { Type = TransformationType.Scale },
+                    new Transformation() { Type = TransformationType.Fade });
             }
 
             InputManager.OnDown += InputManager_OnDown;
+            InputManager.OnMove += InputManager_OnMove;
 
             base.Initialize();
         }
 
+        int spacing;
+        void InputManager_OnMove(InputSource source, TrackingPoint trackingPoint)
+        {
+            if (InputManager.IsPressed && spacing++ % 1 == 0)
+                burst(trackingPoint.BasePosition, 20, 0.5f,  1);
+        }
+
         void InputManager_OnDown(InputSource source, TrackingPoint trackingPoint)
         {
-            int burstSpriteCount = 5;
+            burst(trackingPoint.BasePosition, 100, 1, 5);
+        }
 
-            while (burstSpriteCount-- > 0)
+        private void burst(Vector2 pos, float spread, float scale, int count)
+        {
+            while (count-- > 0)
             {
-                int randTime = 500 + (int)(GameBase.Random.NextDouble() * 400);
-                float randX = (float)(GameBase.Random.NextDouble() * 100);
-                float randY = (float)(GameBase.Random.NextDouble() * 100);
+                int randTime = 300 + (int)(GameBase.Random.NextDouble() * 400);
 
-                Vector2 start = trackingPoint.BasePosition;
-                Vector2 end = trackingPoint.BasePosition + new Vector2(randX - 50, randY - 50);
+                Vector2 end = pos;
+
+                if (spread > 0)
+                {
+                    float randX = (float)(GameBase.Random.NextDouble() * spread);
+                    float randY = (float)(GameBase.Random.NextDouble() * spread);
+
+                    end += new Vector2(randX - spread / 2, randY - spread / 2);
+                }
 
                 pSprite burst = burstSprites[nextBurstSprite];
 
-                burst.Transformations.Clear();
-                burst.Transform(new Transformation(start, end, Clock.Time, Clock.Time + randTime, EasingTypes.In));
-                burst.Transform(new Transformation(TransformationType.Scale, 0.8f + 0.4f * (float)GameBase.Random.NextDouble(), 0.4f + 0.4f * (float)GameBase.Random.NextDouble(), Clock.Time, Clock.Time + randTime, EasingTypes.In));
-                burst.Transform(new Transformation(TransformationType.Fade, (float)GameBase.Random.NextDouble(), 0, Clock.Time, Clock.Time + randTime));
+                for (int i = 0; i < 3; i++)
+                {
+                    Transformation t = burst.Transformations[i];
+
+                    t.StartTime = Clock.Time;
+                    t.EndTime = Clock.Time + randTime;
+
+                    switch (i)
+                    {
+                        case 0:
+                            t.StartVector = pos;
+                            t.EndVector = end;
+                            break;
+                        case 1:
+                            t.StartFloat = scale * 0.8f + 0.4f * (float)GameBase.Random.NextDouble();
+                            t.EndFloat = scale * 0.4f + 0.4f * (float)GameBase.Random.NextDouble();
+                            break;
+                        case 2:
+                            t.StartFloat = (float)GameBase.Random.NextDouble() * 0.6f;
+                            t.EndFloat = 0;
+                            break;
+                    }
+                }
 
                 nextBurstSprite = (nextBurstSprite + 1) % burstSprites.Count;
             }
@@ -59,6 +102,7 @@ namespace osum.Graphics
         public override void Dispose()
         {
             InputManager.OnDown -= InputManager_OnDown;
+            InputManager.OnMove -= InputManager_OnMove;
 
             base.Dispose();
         }
