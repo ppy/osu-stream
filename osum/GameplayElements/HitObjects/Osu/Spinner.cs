e03f4793671b54b97a8502c2b19f5b08871e33b3
Dean Herbert <pe@ppy.sh>
2011-06-24 14:10:55 +0800

    Fix spinner bonus getting too big just after passing.

--- osum/GameplayElements/HitObjects/Osu/Spinner.cs
+++ osum/GameplayElements/HitObjects/Osu/Spinner.cs
@@ -320,6 +320,7 @@ namespace osum.GameplayElements
                 {
                     score = ScoreChange.SpinnerSpinPoints;
                     lastScoredRotationCount += sensitivity_modifier / 4;
+                    lastSamplePlayedRotationCount = currentRotationCount;
                 }
             }
 
