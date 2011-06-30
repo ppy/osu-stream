36479495e47d4cac715449e284f1bca6c62a3b34
Dean Herbert <pe@ppy.sh>
2011-06-30 13:03:56 +0800

    Rewrite low-level input handling to not be so horribly hacky.

--- osum/Input/Sources/InputSourceIphone.cs
+++ osum/Input/Sources/InputSourceIphone.cs
@@ -13,27 +13,11 @@ namespace osum
 			this.gameWindow = gameWindow;
 		}
 
-		private static List<UITouch> NSSetToList(NSSet @set)
-		{
-			// Make full-aot aware of the needed ICollection<UITouch> types
-			ICollection<UITouch> touches_col = (ICollection<UITouch>)@set.ToArray<UITouch>();
-			
-			#pragma warning disable 0219
-			// this is a tiny hack, make sure the AOT compiler knows about
-			// UICollection.Count, as it will need it when we instantiate the list below
-			int touches_count = touches_col.Count;
-			#pragma warning restore 0219
-			
-			List<UITouch> touches = new List<UITouch>(touches_col);
-			
-			return touches;
-		}
-
 		public void HandleTouchesBegan(NSSet touches, UIEvent evt)
 		{
 			TrackingPoint newPoint = null;
-			
-			foreach (UITouch u in NSSetToList(touches)) {
+
+			foreach (UITouch u in touches.ToArray<UITouch>()) {
 				newPoint = new TrackingPointIphone(u.LocationInView(gameWindow), u);
 				trackingPoints.Add(newPoint);
 				TriggerOnDown(newPoint);
@@ -46,7 +30,7 @@ namespace osum
 		{
 			TrackingPoint point = null;
 
-			foreach (UITouch u in NSSetToList(touches)) {
+			foreach (UITouch u in touches.ToArray<UITouch>()) {
 				point = trackingPoints.Find(t => t.Tag == u);
 				if (point != null)
 				{
@@ -60,7 +44,7 @@ namespace osum
 		{
 			TrackingPoint point = null;
 			
-			foreach (UITouch u in NSSetToList(touches)) {
+			foreach (UITouch u in touches.ToArray<UITouch>()) {
 				point = trackingPoints.Find(t => t.Tag == u);
 				if (point != null)
 				{
