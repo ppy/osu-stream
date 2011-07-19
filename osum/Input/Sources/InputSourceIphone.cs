6f5c310c5a603931d4923f28521028e3ef5a3dc5
Dean Herbert <pe@ppy.sh>
2011-07-19 17:59:30 +0800

    Slight input handling optimisation.

--- osum/Input/Sources/InputSourceIphone.cs
+++ osum/Input/Sources/InputSourceIphone.cs
@@ -8,51 +8,62 @@ namespace osum
 {
 	public class InputSourceIphone : InputSource
 	{
-		public InputSourceIphone() : base()
+        public InputSourceIphone() : base()
 		{
 		}
 
         public void HandleTouches(NSSet touches)
         {
             Clock.Update(true);
-            TrackingPoint point = null;
 
-            foreach (UITouch u in touches.ToArray<UITouch>())
+
+            if (touches.Count == 1)
+                handleUITouch((UITouch)touches.AnyObject);
+            else
+                foreach (UITouch u in touches.ToArray<UITouch>())
+                    handleUITouch(u);
+        }
+
+        Dictionary<UITouch,TrackingPoint> touchDictionary = new Dictionary<UITouch, TrackingPoint>();
+
+        private void handleUITouch(UITouch u)
+        {
+
+            TrackingPoint point = null;
+            switch (u.Phase)
             {
-                switch (u.Phase)
-                {
-                    case UITouchPhase.Began:
-                        if (AppDelegate.UsingViewController) return;
-                        point = new TrackingPointIphone(u.LocationInView(EAGLView.Instance), u);
-                        trackingPoints.Add(point);
-                        TriggerOnDown(point);
-                        break;
-                    case UITouchPhase.Cancelled:
-                    case UITouchPhase.Ended:
-                        point = trackingPoints.Find(t => t.Tag == u);
-                         if (point != null)
-                         {
-                             trackingPoints.Remove(point);
-                             TriggerOnUp(point);
-                         }
-                        break;
-                    case UITouchPhase.Moved:
-                        point = trackingPoints.Find(t => t.Tag == u);
-                        if (point != null)
-                        {
-                            point.Location = u.LocationInView(EAGLView.Instance);
-                            TriggerOnMove(point);
-                        }
-                        break;
-                }
+                case UITouchPhase.Began:
+                    if (AppDelegate.UsingViewController) return;
+                    point = new TrackingPointIphone(u.LocationInView(EAGLView.Instance), u);
+                    touchDictionary[u] = point;
+                    trackingPoints.Add(point);
+                    TriggerOnDown(point);
+                    break;
+                case UITouchPhase.Cancelled:
+                case UITouchPhase.Ended:
+                    point = touchDictionary[u];
+                    trackingPoints.Remove(point);
+                    touchDictionary.Remove(u);
+                    TriggerOnUp(point);
+                    break;
+                case UITouchPhase.Moved:
+                    point = touchDictionary[u];
+                    if (point != null)
+                    {
+                        point.Location = u.LocationInView(EAGLView.Instance);
+                        TriggerOnMove(point);
+                    }
+                    break;
             }
         }
+
 
         public void ReleaseAllTouches()
         {
             foreach (TrackingPoint t in trackingPoints)
                 TriggerOnUp(t);
             trackingPoints.Clear();
+            touchDictionary.Clear();
         }
 	}
 }
