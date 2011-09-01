5fd30307fbc1ae093ef3609138112e37309ba07d
Dean Herbert <pe@ppy.sh>
2011-09-01 19:04:00 +0800

    Limit length of beatmap preview.

--- osum/GameplayElements/HitObjectManagerPreview.cs
+++ osum/GameplayElements/HitObjectManagerPreview.cs
@@ -24,6 +24,20 @@ namespace osum.GameplayElements
                     newStreamSwitchPoints.Add(beatmap.StreamSwitchPoints[i]);
             beatmap.StreamSwitchPoints = newStreamSwitchPoints;
 
+            int pointCount = beatmap.StreamSwitchPoints.Count;
+
+            int lastObjectTime = -1;
+            if (pointCount > 3)
+                lastObjectTime = beatmap.StreamSwitchPoints[Math.Min(pointCount - 1, 4)];
+
+            if (lastObjectTime > 0)
+                foreach (List<HitObject> objects in StreamHitObjects)
+                    foreach (HitObject h in objects.FindAll(h => h.StartTime > lastObjectTime))
+                    {
+                        h.Sprites.ForEach(s => s.Bypass = true);
+                        objects.Remove(h);
+                    }
+
             base.PostProcessing();
         }
 
@@ -37,7 +51,5 @@ namespace osum.GameplayElements
 
         public override bool IsLowestStream { get { return ActiveStream == Difficulty.Easy; } }
         public override bool IsHighestStream { get { return ActiveStream == Difficulty.Expert; } }
-
-
     }
 }
