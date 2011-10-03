1a3e9a208fc8810704cb61d5d30df3600ece2528
Dean Herbert <pe@ppy.sh>
2011-10-03 10:20:24 +0800

    Remove unused osu!common library.

--- osum/osu!common/Libraries/NetLib/HttpRequestEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace osu_common.Libraries.NetLib
-{
-    using System;
-    using System.Runtime.CompilerServices;
-
-    public delegate void HttpRequestEventHandler(object sender, HttpRequestEventArgs e);
-}
-
