9e27f2428d1d389549a2b9098e14cee0bc90087e
Dean Herbert <pe@ppy.sh>
2012-03-28 09:56:34 +0800

    Fix streamTester running at retina craziness.

--- osum/Support/Desktop/GameWindowDesktop.cs
+++ osum/Support/Desktop/GameWindowDesktop.cs
@@ -20,7 +20,7 @@ namespace osum
     {
         /// <summary>Creates a 1024x768 window with the specified title.</summary>
         public GameWindowDesktop()
-            : base(2048, 1536, GraphicsMode.Default, "osu!stream")
+            : base(480, 320, GraphicsMode.Default, "osu!stream")
         {
             VSync = VSyncMode.On;
             //GameBase.WindowSize = new Size(960,640);
