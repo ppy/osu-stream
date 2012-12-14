d170cd872d65f493f79f9ae73e9670e71cd11da7
Dean Herbert <pe@ppy.sh>
2012-12-14 12:13:12 +0800

    Merge fix.

---
+++ osu!stream/Main.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+namespace osum
+{
+    public class Application
+    {
+        static void Main(string[] args)
+        {
+#if iOS
+            GameBase game = new GameBaseIphone();
+            game.Run();
+#else
+            GameBase game = new GameBaseDesktop();
+            game.Run();
+#endif
+        }
+    }
+}
+
