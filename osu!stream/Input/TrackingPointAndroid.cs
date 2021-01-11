ac1ed519074ac3a782ea47b3eb34aea92ea963bb
Natalie P <cyuubiapps@gmail.com>
2021-01-11 10:38:35 -0500

    Fix position

--- osu!stream/Input/TrackingPointAndroid.cs
+++ osu!stream/Input/TrackingPointAndroid.cs
@@ -3,19 +3,24 @@ using OpenTK;
 
 namespace osum.Input
 {
+    // based on TrackingPointIphone.
     class TrackingPointAndroid : TrackingPoint
     {
+        private const bool FIX_POSITION = true;
+
         public TrackingPointAndroid(PointF location, object tag) : base(location, tag)
         {
         }
 
         public override void UpdatePositions()
         {
-            float x = Location.X * GameBase.ScaleFactor;
-            float y = Location.Y * GameBase.ScaleFactor;
+            float x = (FIX_POSITION ? Location.X : Location.Y) * GameBase.ScaleFactor;
+            float y = (FIX_POSITION ? Location.Y : Location.X) * GameBase.ScaleFactor;
 
-            if (GameBase.Instance.FlipView)
+            if (GameBase.Instance.FlipView || FIX_POSITION)
                 y = GameBase.NativeSize.Height - y;
+            if (GameBase.Instance.FlipView && !FIX_POSITION)
+                x = GameBase.NativeSize.Width - x;
 
             Vector2 oldBase = BasePosition;
             BasePosition = new Vector2(
